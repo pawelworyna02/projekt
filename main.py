2be7b790c24b384570c95a40f07a267def96f1fd
pawelworyna02 <149298074+pawelworyna02@users.noreply.github.com>
2023-10-29 20:44:44 +0100

    dodanie możliwości podawania literek

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 
-def generuj_histogram(tekst):
+def generuj_histogram(tekst, litery_wlasne=None):
     histogram = {}
     for znak in tekst:
-        if znak.isalpha():
+        if znak.isalpha() and (not litery_wlasne or znak.lower() in litery_wlasne):
             znak = znak.lower()
             if znak in histogram:
                 histogram[znak] += 1
@@ -25,10 +25,10 @@ def zapisz_histogram_jako_obraz(histogram, nazwa_pliku):
     plt.title('Histogram Częstotliwości Liter')
     plt.savefig(nazwa_pliku, format='png')
 
-def generuj_histogram_z_pliku_tekstowego(sciezka_pliku):
+def generuj_histogram_z_pliku_tekstowego(sciezka_pliku, litery_wlasne=None):
     with open(sciezka_pliku, 'r') as plik:
         tekst = plik.read()
-        histogram = generuj_histogram(tekst)
+        histogram = generuj_histogram(tekst, litery_wlasne)
 
         print(f"Histogram dla pliku {sciezka_pliku}:")
         for znak, ilosc in histogram.items():
@@ -42,7 +42,10 @@ def main():
     for nazwa_pliku in os.listdir(katalog_danych):
         if nazwa_pliku.endswith(".txt"):
             sciezka_pliku = os.path.join(katalog_danych, nazwa_pliku)
-            generuj_histogram_z_pliku_tekstowego(sciezka_pliku)
+            litery_wlasne = input("Podaj zestaw liter oddzielonych przecinkami: ")
+            litery_wlasne = litery_wlasne.split(',') if litery_wlasne else None
+
+            generuj_histogram_z_pliku_tekstowego(sciezka_pliku, litery_wlasne)
 
 if __name__ == "__main__":
     main()
